f3f59173a050758aade0d9715528f195b993f287
Renny Muragu <118157651+rennynjuguna@users.noreply.github.com>
2023-01-31 16:24:40 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
